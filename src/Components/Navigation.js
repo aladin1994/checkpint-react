75e2f9cd98bc7b867ddfbd50f23057d0509051c1
aladin1994 <aladinlfig@gmail.com>
2023-04-27 13:54:45 +0100

    commit

---
+++ src/Components/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+const Navigation = () => {
+  return (
+    <div>
+        <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Navigation
